9b830947a1a1e9dba6471a51713dccb8a7dd2c69
Mouyu9230 <2313228276@qq.com>
2026-07-27 21:28:57 +0800

    main update

--- server/server.cpp
+++ server/server.cpp
@@ -7,6 +7,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <utility>
 
 #include "../protocol/protocol.hpp"
 #include "../network/socket/socket.hpp"
@@ -15,6 +16,8 @@
 #include "../network/recv_send/recv_send.hpp"
 #include "../network/thread_pool/thread_pool.hpp"
 
+static volatile bool g_running = true;
+
 int main(int argc,char* argv[]){
 
     const char* ip="0.0.0.0";
@@ -22,7 +25,7 @@ int main(int argc,char* argv[]){
     if(argc>1)port=std::atoi(argv[1]);
     if(argc>2)ip=argv[2];
 
-    connection_init();
+    connection_init();   
     //fpr
     network::Socket listen_sock;
 
@@ -31,6 +34,36 @@ int main(int argc,char* argv[]){
         return 1;
     }
     listen_sock.set_nonblock();
+    if(listen_sock.create_server(ip,port)!=0){
+        //fpr
+        return 1;
+    }
+
+    network::Epoll epoll_main(1024);
+    epoll_main.add(listen_sock.fd(),EPOLLIN);
+    //fpr
+
+     ThreadPool pool;
+     pool.start([](Task task)->TaskResult{
+
+        //根据tasktype分发任务 返回result
+
+        return TaskResult{task.fd,std::move(task.data),false};
+     });
+     //fpr
+
+     recv_send rs_tool;
+
+     while(g_running){
+
+        
+
+
+
+
+     }
+
+
 
 
 
